e08daacd8d6b78cd0f71b02888868aa28d7839d7
William French <willum070@yahoo.com>
2014-08-26 12:53:17 -0700

    updates to ma blog

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 cereal
 ======
 
-A super-simple C# console app for testing serial I/O.
+A super-simple C# console app for testing serial I/O. I wrote Cereal to be very simple and yet provide some nice functionality so that it can be extended to do more stuff. Here is what Cereal does:
+
+- Opens a serial connection to the specified COM port.
+- Monitors that COM port using an event handler, printing the data to the console.
+- Accepts user input for the available commands. The actual commands are defined in the corresponding Arduino sketch.
+
--- arduino_sketch/LightSensorExample/LightSensorExample.ino
+++ arduino_sketch/LightSensorExample/LightSensorExample.ino
@@ -3,9 +3,10 @@
 */
 
 //int led = 13;
-int tempt6000Pin = 0;
+int temt6000Pin = 0;
 int numCharRead = 0;
 char incomingData[20];
+//int value = analogRead(temt6000Pin);
 
 // the setup routine runs once when you press reset:
 void setup() {                
@@ -24,34 +25,42 @@ void loop() {
     }
 }
 
+void sendTemps() {
+  Serial.print('[');
+  Serial.print(analogRead(temt6000Pin));
+  Serial.println(']');
+}
+
 int processCommands(int numCharRead ) {
   
-    if (numCharRead < 2)  { //need at least '[' and one other character
-        Serial.print("[!Badly formatted command]");  //! to indicate text string
-        return -1; //skip the rest
-    }
+   if (numCharRead < 2)  { //need at least '[' and one other character
+       Serial.print("[!Badly formatted command]");  //! to indicate text string
+       return -1; //skip the rest
+     }
   
     //everything after '[' to the length read is the command.
     if (incomingData[0] != '[') {
-        Serial.print("[!Badly formatted command]");  //! to indicate text string
-        return -1; //skip the rest
+       Serial.print("[!Badly formatted command]");  //! to indicate text string
+       return -1; //skip the rest
     }
   
     //switch based on first character after the '['
-    switch(incomingData[1]) {
+      switch(incomingData[1]) {
         
-        // Return the device ID string.
         case '?':
-            Serial.print("My_Arduino_1234567890]");
-             break;
+         Serial.print("[MyArduino_1234567890]");
+        break;
         
-        // Return the sensor reading.
-        case 'get':
-            int value = analogRead(temt6000Pin);
-            Serial.println(value);
-            break;
+        ///ADD COMMANDS RELEVANT TO YOUR DEVICE & DRIVER HERE
+        //example for dummy
+        case 'v':
+         Serial.print('[');
+         Serial.print('value');
+         Serial.print(']');     
+         break;
         
         default:
-            Serial.print("[!No matching command]");
-    }
+          Serial.print("[!No matching command]");
+      }
+
 }
