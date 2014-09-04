3ea4b887ba073f6ad5a48c188916c2204a6c03bc
William French <willum070@yahoo.com>
2014-09-03 19:47:35 -0700

    I just can't stop it.

---
+++ arduino_sketch/LightSensorExample_3/LightSensorExample_3.ino
@@ -0,0 +1,84 @@
+/*
+  Light sensor example.
+*/
+
+int led = 13;
+int temt6000Pin = 0;
+int numCharRead = 0;
+char incomingData[20];
+boolean stream = false;
+boolean light = false;
+// the setup routine runs once when you press reset:
+void setup() {                
+    //Setup the serial port
+    Serial.begin(9600);
+    pinMode(led, OUTPUT);
+}
+
+// Infinite loop to get user input:
+void loop() {
+    if (Serial.available() > 0) {
+        numCharRead = Serial.readBytesUntil(']',  incomingData, 19);
+        processCommands(numCharRead);        
+    }
+    
+    // Send continuously if the 'stream' flag is set to 'true'.
+    if(stream) {
+      sendReading();
+      delay(100);
+    }
+    
+    if(analogRead(temt6000Pin) < 30)
+       
+}
+
+void light() {
+  
+}
+
+// Send out a reading.
+void sendReading() {
+  Serial.print("[");
+  Serial.print(analogRead(temt6000Pin));
+  Serial.print("]");
+}
+
+int processCommands(int numCharRead ) {
+  
+   if (numCharRead < 2)  { //need at least '[' and one other character
+       Serial.print("[!Badly formatted command]");  //! to indicate text string
+       return -1; //skip the rest
+     }
+  
+    //everything after '[' to the length read is the command.
+    if (incomingData[0] != '[') {
+       Serial.print("[!Badly formatted command]");  //! to indicate text string
+       return -1; //skip the rest
+    }
+  
+    //switch based on first character after the '['
+      switch(incomingData[1]) {
+        
+        case '?':
+         Serial.print("[MyArduino_1234567890]");
+        break;
+        
+        case 'v':
+         Serial.print('[');
+         Serial.print(analogRead(temt6000Pin));
+         Serial.print(']');     
+         break;
+         
+       // Toggle continuous data.  
+       case 's':
+         if(!stream)
+           stream = true;
+         else
+           stream = false;
+         break;
+         
+        default:
+          Serial.print("[!No matching command]");
+      }
+
+}
